be66bd41d08f264237bd2b6d25144583a28386f9
rake146 <jake.pinchbeck@hotmail.co.uk>
2019-10-22 12:56:26 +0100

    Showing title + styling bars to add changing colours + displaying widths of the bars according to frequency

--- frontend_exercise_implementation/src/App.js
+++ frontend_exercise_implementation/src/App.js
@@ -17,8 +17,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div classStyle="float: right;">
-          {createHistogram(data)}
+        <div className="complete-histogram">
+          <div className="histogram">
+            <h3>{inputObject.title}</h3>
+            {createHistogram(data)}
+          </div>
+          <div className="bars">
+            {createBars(data)}
+          </div>
         </div>
       </header>
     </div>
@@ -52,14 +58,38 @@ function createHistogram(arr){
   }
 
   for (let i = 0; i < arr.length; i++){
+    histogram.push(
+      <div>
+        <span>
+            <div className="key">{arr[i].key}</div>
+            <div className="val"> {arr[i].value}</div><br></br>
+        </span>
+      </div>
+    );
+  }
+
+  return histogram;
+}
+
+function createBars(arr){
+  let histogram = [];
+
+  // calculate widths;
+  let longestWidth = 200;
+  let highestValue = arr[0].value;
+  let widthArr = [];
+
+  for(let i = 0; i < arr.length; i++){
+    widthArr.push((arr[i].value / highestValue));
+  }
+
+  for (let i = 0; i < arr.length; i++){
     let divStyle = {
-      width: widthArr[i] * longestWidth + 'px'
+      width: widthArr[i] * longestWidth + 'px',
+      backgroundColor: 'rgb(201, 76,' + (1/widthArr[i]) * 100 + ')'
     };
     histogram.push(
-      <span>
-        {arr[i].key} {arr[i].value}
-        <div className="bar" style={divStyle}></div><br></br>
-      </span>
+        <span><div className="bar" style={divStyle}></div><br></br></span>
     );
   }
 
