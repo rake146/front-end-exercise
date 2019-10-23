ea0e6fe5ed99f3df0267107e92ad6c7d779eba98
rake146 <jake.pinchbeck@hotmail.co.uk>
2019-10-23 16:42:19 +0100

    Mocking api call for data to be fetched

---
+++ frontend_exercise_implementation/buildScripts/mockDataSchema.js
@@ -0,0 +1,27 @@
+var schema = {
+    "histogram-data": [{
+        "title": "Pets in Cambridge",
+        "data": {
+          "cat": 55,
+          "dog": 14,
+          "lizard": 37, 
+          "elephant": 3,
+          "geese": 14,
+          "chimps": 2,
+          "crocs": 5
+        }
+      },
+      {
+        "title": "Pets in Africa",
+        "data": {
+          "lions": 10,
+          "tigers": 2,
+          "bears": 15,
+          "giraffe": 11,
+          "toucans": 2,
+          "elephant": 9
+        }
+      }]
+  };
+  
+  module.exports = schema;
