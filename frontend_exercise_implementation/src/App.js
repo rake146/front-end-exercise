c6aa972417ecc913f2e093e644142272a0fb8c9b
rake146 <jake.pinchbeck@hotmail.co.uk>
2019-10-23 16:40:04 +0100

    Abstract histogram away from App and place in own component

--- frontend_exercise_implementation/src/App.js
+++ frontend_exercise_implementation/src/App.js
@@ -1,99 +1,77 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import Histogram from './components/histogram'
 
-let inputObject = {
-  "title": 'Pets in Cambridge',
-  "data": {
-    "cat": 55,
-    "dog": 14,
-    "lizard": 37, // There are a lot of Liz lovers in Cambridge. :D
-    "elephant": 3
-  }
-}
-
-function App() {
-  let data = sortObject();
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <div className="complete-histogram">
-          <div className="histogram">
-            <h3>{inputObject.title}</h3>
-            {createHistogram(data)}
-          </div>
-          <div className="bars">
-            {createBars(data)}
-          </div>
-        </div>
-      </header>
-    </div>
-  );
-}
-
-function sortObject(){
-  let arr = [];
-
-  // generate array from js Obj
-  Object.keys(inputObject.data).forEach(function(key){
-    arr.push({"key" : key, "value" : inputObject.data[key]});
-  });
-
-  // sort values
-  arr.sort(function(a, b){ return b.value - a.value;});
-
-  return arr;
-}
-
-function createHistogram(arr){
-  let histogram = [];
-
-  // calculate widths;
-  let longestWidth = 200;
-  let highestValue = arr[0].value;
-  let widthArr = [];
-
-  for(let i = 0; i < arr.length; i++){
-    widthArr.push((arr[i].value / highestValue));
+export default class App extends Component {
+  state = {
+    histograms: []
   }
 
-  for (let i = 0; i < arr.length; i++){
-    histogram.push(
-      <div>
-        <span>
-            <div className="key">{arr[i].key}</div>
-            <div className="val"> {arr[i].value}</div><br></br>
-        </span>
+  histogramData = [{
+    "title": 'Pets in Cambridge',
+    "data": {
+      "cat": 55,
+      "dog": 14,
+      "lizard": 37, // There are a lot of Liz lovers in Cambridge. :D
+      "elephant": 3,
+      "geese": 14,
+      "chimps": 2,
+      "crocs": 5, // There aren't many crocodile lovers D:
+    }
+  },
+  {
+    "title": 'Pets in Africa',
+    "data": {
+      "lions": 10,
+      "tigers": 2,
+      "bears": 15,
+      "giraffe": 11,
+      "toucans": 2,
+      "elephant": 9
+    }
+  }]
+
+  componentDidMount() {
+    // fetch histograms from API
+    fetch('/histogram-data')
+    .then(res => res.json())
+    .then((data) => {
+      this.setState({ histograms: data })
+      console.log("DATA", data);
+    })
+    .catch(console.log)
+
+    console.log(this.state);
+  }
+
+  render(){
+    // would typically use state.histograms[0] here instead of histogramData if endpoint was live
+    return (
+      <div className="App">
+        <header className="App-header"> 
+          <Histogram obj={this.histogramData[0]} color='#F06D06' maxBarWidth={200}/>
+          <Histogram obj={this.histogramData[1]} color='#F065FA' maxBarWidth={200}/>
+        </header>
       </div>
     );
   }
-
-  return histogram;
 }
 
-function createBars(arr){
-  let histogram = [];
+/*
+ API interface specification:
 
-  // calculate widths;
-  let longestWidth = 200;
-  let highestValue = arr[0].value;
-  let widthArr = [];
-
-  for(let i = 0; i < arr.length; i++){
-    widthArr.push((arr[i].value / highestValue));
-  }
-
-  for (let i = 0; i < arr.length; i++){
-    let divStyle = {
-      width: widthArr[i] * longestWidth + 'px',
-      backgroundColor: 'rgb(201, 76,' + (1/widthArr[i]) * 100 + ')'
-    };
-    histogram.push(
-        <span><div className="bar" style={divStyle}></div><br></br></span>
-    );
-  }
-
-  return histogram;
-}
-
-export default App;
+ array of histograms e.g.
+ 
+ "histogram-data": [{
+  "title": "Pets in Cambridge",
+  "data": {
+    "cat": 55,
+    "dog": 14
+  },
+  {
+  "title": "Pets in Africa",
+  "data": {
+    "Lions": 2,
+    "Tigers": 3
+  }]
+*/
